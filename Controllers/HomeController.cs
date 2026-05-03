9f08c160f7766a50e818d168d75c4fc6bb144edb
Alper <alperbah@gmail.com>
2026-05-03 18:26:12 +0300

    Güncelle gösterge paneli tasarımını

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Kumburgaz.Web.Data;
 using Microsoft.AspNetCore.Mvc;
 using Kumburgaz.Web.Models;
+using Kumburgaz.Web.Services;
 
 namespace Kumburgaz.Web.Controllers;
 
@@ -13,6 +14,16 @@ public class HomeController(ApplicationDbContext db) : Controller
         ViewBag.TotalGenerated = db.DuesInstallments.Sum(x => x.Amount);
         ViewBag.TotalCollections = db.Collections.Sum(x => x.Amount);
         ViewBag.BillingGroups = db.BillingGroups.Count(x => x.Active);
+        ViewBag.ActiveUnits = db.Units.Count(x => x.Active);
+        ViewBag.CombinedUnits = db.Units.Count(x => x.Active && x.IsCombined);
+        ViewBag.OpenInstallments = db.DuesInstallments.Count(x => x.RemainingAmount > 0);
+        ViewBag.PaidInstallments = db.DuesInstallments.Count(x => x.RemainingAmount <= 0);
+        ViewBag.LedgerIncome = db.LedgerTransactions
+            .Where(x => x.IncomeExpenseCategory != null && x.IncomeExpenseCategory.Type == CategoryTypeHelper.Gelir)
+            .Sum(x => x.Amount);
+        ViewBag.LedgerExpense = db.LedgerTransactions
+            .Where(x => x.IncomeExpenseCategory != null && x.IncomeExpenseCategory.Type == CategoryTypeHelper.Gider)
+            .Sum(x => x.Amount);
         return View();
     }
 
